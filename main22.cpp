26e2ca230f9976bbc0bdc7a051eab2df35d3a8d4
OlegKondr <125397427+OlegKondr@users.noreply.github.com>
2023-03-15 18:23:47 +0300

    Add files via upload

---
+++ main22.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+main() 
+{
+    int pin1,pin2;
+printf("pin1=");
+scanf("%i",&pin1);
+printf("pin2=");
+scanf("%i",&pin2);
+if((((pin1==123)&&(pin2==321))||((pin1==456)&&(pin2==654))||((pin1==753)&&(pin2==357))))
+{
+ printf("OK");
+}
+else
+{
+	printf("ERROR");
+		}
+	}
+
